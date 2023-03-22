c7c1c342a7ed61006a183ee760e66bac00e9df40
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-03-22 14:59:42 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-const products_routes = require("./routes/products")
+const products_routes = require("./routes/products");
 
 app.get("/", (req, res) => {
   res.send("I am live");
 });
 
 // middleware
-app.use("/api/products", products_routes)
+app.use("/api/products", products_routes);
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGODB_URL);
     app.listen(PORT, () => {
       console.log(`Connected at PORT:${PORT}`);
     });
